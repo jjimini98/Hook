667d1df424d92713d2871a83842531f680b30433
Jimin <jjimini98@naver.com>
2024-12-15 17:20:47 +0900

    Initial Commit

---
+++ Hook Watch App/HookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HookApp.swift
+//  Hook Watch App
+//
+//  Created by Jimin Yoo on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Hook_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hook Watch AppTests/Hook_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hook_Watch_AppTests.swift
+//  Hook Watch AppTests
+//
+//  Created by Jimin Yoo on 12/15/24.
+//
+
+import Testing
+@testable import Hook_Watch_App
+
+struct Hook_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
